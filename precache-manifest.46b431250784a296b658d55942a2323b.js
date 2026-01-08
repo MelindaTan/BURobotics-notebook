ba8645993d0b5feb6ab11f6773179c88b9c46db2
Deploy <deploy@github.local>
2026-01-07 19:50:21 -0800

    Add 404.html redirect for GitHub Pages SPA routing

---
+++ precache-manifest.46b431250784a296b658d55942a2323b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c35bd600d5878a2b424c0d69364f087",
+    "url": "/BURobotics-notebook/index.html"
+  },
+  {
+    "revision": "35aa611cbbac6f6f901d",
+    "url": "/BURobotics-notebook/static/js/2.fc841a4a.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/BURobotics-notebook/static/js/2.fc841a4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02c3768d0bf1d40400d1",
+    "url": "/BURobotics-notebook/static/js/main.34bad98d.chunk.js"
+  },
+  {
+    "revision": "7f6b494203621e66424c",
+    "url": "/BURobotics-notebook/static/js/runtime-main.66a55cd7.js"
+  },
+  {
+    "revision": "fae0e57fe07560137edcf6df6d51ca86",
+    "url": "/BURobotics-notebook/static/media/post_1.fae0e57f.png"
+  },
+  {
+    "revision": "500a0a51ad1a47240eccc34651ed9977",
+    "url": "/BURobotics-notebook/static/media/sample.500a0a51.md"
+  }
+]);
