83ed79ca6e24bb03da7e8236753b5e35fed393c2
MelindaTran <meltran@bu.edu>
2026-01-07 20:13:07 -0800

    Button: render styled Button as Link/anchor/button to avoid nested interactive elements

--- src/components/Utility/Button.jsx
+++ src/components/Utility/Button.jsx
@@ -1,34 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from '../StyledComponents/StyledComponents';
+import { Button as StyledButton } from '../StyledComponents/StyledComponents';
 
 const PrimaryButton = (props) => {
     const isExternal = props.newTab || (props.route && (props.route.indexOf('http://') === 0 || props.route.indexOf('https://') === 0));
 
-    const content = (
-        <Button color={props.color} onClick={props.onClick} type={props.submit ? "submit" : "button"} right={props.right ? true : false} left={props.left ? true : false}>
-            {props.left ? <i className="las la-arrow-left"></i> : null}
+    const inner = (
+        <>
+            {props.left ? <i className="las la-arrow-left" /> : null}
             <span>{props.text}</span>
-            {props.right ? <i className={`las la-${props.newTab ? 'level-up-alt' : 'arrow-right'}`}></i> : null}
-        </Button>
+            {props.right ? <i className={`las la-${props.newTab ? 'level-up-alt' : 'arrow-right'}`} /> : null}
+        </>
     );
 
+    // No route: plain button (useful for form submits)
     if (!props.route) {
-        return content;
-    }
-
-    if (isExternal) {
         return (
-            <a href={props.route} target={props.newTab ? '_blank' : null} rel={props.newTab ? 'noopener noreferrer' : null}>
-                {content}
-            </a>
+            <StyledButton
+                color={props.color}
+                onClick={props.onClick}
+                type={props.submit ? 'submit' : 'button'}
+                right={props.right ? true : false}
+                left={props.left ? true : false}
+            >
+                {inner}
+            </StyledButton>
         );
     }
 
+    // External link: render as anchor
+    if (isExternal) {
+        return (
+            <StyledButton
+                as="a"
+                href={props.route}
+                target={props.newTab ? '_blank' : null}
+                rel={props.newTab ? 'noopener noreferrer' : null}
+                color={props.color}
+                right={props.right ? true : false}
+                left={props.left ? true : false}
+            >
+                {inner}
+            </StyledButton>
+        );
+    }
+
+    // Internal route: render the styled button as a Router Link (no nested anchors)
     return (
-        <Link to={props.route}>
-            {content}
-        </Link>
+        <StyledButton
+            as={Link}
+            to={props.route}
+            color={props.color}
+            right={props.right ? true : false}
+            left={props.left ? true : false}
+        >
+            {inner}
+        </StyledButton>
     );
 }
 
